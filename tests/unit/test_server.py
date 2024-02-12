833d7c28e5b8cec97dd3feb2eabbda4019006b6f
rlouvrie <rlouvrie@student.42.fr>
2024-02-12 17:21:53 +0100

    updating the test adding the encoding for ' char

--- tests/unit/test_server.py
+++ tests/unit/test_server.py
@@ -18,5 +18,5 @@ class TestShowSummary:
         invalid_email = "invalid@test.com"
         response = client.post('/showSummary', data={'email': invalid_email}, follow_redirects=True)
         assert response.status_code == 200
-        assert "Sorry, that email wasn't found." in response.get_data(as_text=True)
+        assert "Sorry, that email wasn&#39;t found." in response.get_data(as_text=True)
 
