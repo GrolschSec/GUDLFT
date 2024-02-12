2bf2d3fa089c39a62281bd0792e51b6a228b46aa
rlouvrie <rlouvrie@student.42.fr>
2024-02-12 19:34:05 +0100

    Correcting bug where clubs can spend more points than they have, see OpenClassrooms-Student-Center/Python_Testing#2

--- server.py
+++ server.py
@@ -52,8 +52,11 @@ def purchasePlaces():
     ]
     club = [c for c in clubs if c["name"] == request.form["club"]][0]
     placesRequired = int(request.form["places"])
-    competition["numberOfPlaces"] = int(competition["numberOfPlaces"]) - placesRequired
-    flash("Great-booking complete!")
+    if placesRequired > int(club["points"]):
+        flash("You don't have enough points to complete booking!")
+    else:
+        competition["numberOfPlaces"] = int(competition["numberOfPlaces"]) - placesRequired
+        flash("Great-booking complete!")
     return render_template("welcome.html", club=club, competitions=competitions)
 
 
