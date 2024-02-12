3b36f058a2ba3e895d645db06c09f2a00706372b
rlouvrie <rlouvrie@student.42.fr>
2024-02-12 20:03:35 +0100

    adding test to reproduce the error

--- tests/unit/test_server.py
+++ tests/unit/test_server.py
@@ -1,2 +1,31 @@
 import pytest
-from server import app
+
+
+class TestPurchasePlaces:
+    def test_purchase_places_less_than_12_places(self, client):
+        response = client.post(
+            "/purchasePlaces",
+            data={
+                "competition": "Spring Festival",
+                "club": "Simply Lift",
+                "places": "6",
+            },
+        )
+        assert response.status_code == 200
+        assert "Great-booking complete!" in response.get_data(as_text=True)
+
+    def test_purchase_places_more_than_12_places(self, client):
+        response = client.post(
+            "/purchasePlaces",
+            data={
+                "competition": "Spring Festival",
+                "club": "Simply Lift",
+                "places": "13",
+            },
+        )
+        assert response.status_code == 200
+        assert "Great-booking complete!" not in response.get_data(as_text=True)
+        assert (
+            "You can&#39;t buy more than 12 places per competition!"
+            in response.get_data(as_text=True)
+        )
