7a3dd3cffcb5247f8e8594cd891f8f55abaec130
rlouvrie <rlouvrie@student.42.fr>
2024-02-12 18:35:42 +0100

    adding setup for purchasePlaces

--- tests/unit/test_server.py
+++ tests/unit/test_server.py
@@ -1,2 +1,11 @@
 import pytest
-from server import app
+
+@pytest.fixture.usefixtures('client', 'competitions', 'clubs')
+class TestPurchasePlaces:
+	
+	def set_up(self, monkeypatch, competitions, clubs):
+		monkeypatch.setattr('server.competitions', competitions)
+		monkeypatch.setattr('server.clubs', clubs)
+	
+	def test_purchase_places_valid_amount(self, client):
+		pass
