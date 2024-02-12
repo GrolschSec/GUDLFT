4991e08bd079eee38dff6ca32c93f9c5c3e47039
rlouvrie <rlouvrie@student.42.fr>
2024-02-12 22:04:50 +0100

    adding one more test

--- tests/unit/test_server.py
+++ tests/unit/test_server.py
@@ -29,3 +29,24 @@ class TestPurchasePlaces:
             "You can&#39;t buy more than 12 places per competition!"
             in response.get_data(as_text=True)
         )
+
+    def test_purchase_places_more_than_12_in_2_times(self, client):
+        data = {
+                "competition": "Spring Festival",
+                "club": "Simply Lift",
+                "places": "7",
+        }
+        client.post(
+            "/purchasePlaces",
+            data=data,
+        )
+        response = client.post(
+            "/purchasePlaces",
+            data=data,
+        )
+        assert response.status_code == 200
+        assert "Great-booking complete!" not in response.get_data(as_text=True)
+        assert (
+            "You can&#39;t buy more than 12 places per competition!"
+            in response.get_data(as_text=True)
+        )
