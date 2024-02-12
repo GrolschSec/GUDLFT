81bb0337f11bb307994f56cb091ba4e5ef4bff2e
rlouvrie <rlouvrie@student.42.fr>
2024-02-12 19:32:58 +0100

    adding test to reproduce the bug

--- tests/unit/test_server.py
+++ tests/unit/test_server.py
@@ -1,11 +1,16 @@
 import pytest
 
-@pytest.fixture.usefixtures('client', 'competitions', 'clubs')
+
 class TestPurchasePlaces:
 	
-	def set_up(self, monkeypatch, competitions, clubs):
-		monkeypatch.setattr('server.competitions', competitions)
-		monkeypatch.setattr('server.clubs', clubs)
-	
 	def test_purchase_places_valid_amount(self, client):
-		pass
+		response = client.post("/purchasePlaces", data={"club": "Iron Temple", "competition": "Spring Festival", "places": "4"})
+		assert response.status_code == 200
+		assert "Great-booking complete!" in response.get_data(as_text=True)
+	
+	def test_purchase_places_invalid_amount(self, client):
+		response = client.post("/purchasePlaces", data={"club": "Iron Temple", "competition": "Spring Festival", "places": "100"})
+		assert response.status_code == 200
+		assert "Great-booking complete!" not in response.get_data(as_text=True)
+		assert "You don&#39;t have enough points to complete booking!" in response.get_data(as_text=True)
+
