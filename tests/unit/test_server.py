94a46b2b5db34de2175af425541e2c0468f6a972
rlouvrie <rlouvrie@student.42.fr>
2024-02-12 18:50:39 +0100

    updating test with common setup for db

--- tests/unit/test_server.py
+++ tests/unit/test_server.py
@@ -1,21 +1,17 @@
 import pytest
 
 
-@pytest.mark.usefixtures('client', 'clubs', 'competitions')
+@pytest.mark.usefixtures("client", "clubs", "competitions")
 class TestShowSummary:
-    @pytest.fixture(autouse=True)
-    def setup(self, monkeypatch, competitions, clubs):
-        monkeypatch.setattr('server.clubs', clubs)
-        monkeypatch.setattr('server.competitions', competitions)
-    
     def test_show_summary_with_valid_email(self, client, clubs):
-        valid_email = next(club['email'] for club in clubs)
-        response = client.post('/showSummary', data={'email': valid_email})
+        valid_email = next(club["email"] for club in clubs)
+        response = client.post("/showSummary", data={"email": valid_email})
         assert response.status_code == 200
 
     def test_show_summary_with_invalid_email(self, client):
         invalid_email = "invalid@test.com"
-        response = client.post('/showSummary', data={'email': invalid_email}, follow_redirects=True)
+        response = client.post(
+            "/showSummary", data={"email": invalid_email}, follow_redirects=True
+        )
         assert response.status_code == 200
         assert "Sorry, that email wasn&#39;t found." in response.get_data(as_text=True)
-
