dbc1641590c235dc2a29b51aa070a955d8b51bdd
rlouvrie <rlouvrie@student.42.fr>
2024-02-13 16:24:45 +0100

    updating conftest, to new global variable from the db and adding test for the bug

--- tests/unit/test_server.py
+++ tests/unit/test_server.py
@@ -1,2 +1,11 @@
 import pytest
-from server import app
+
+class TestPurchasePlaces:
+	def test_purchase_places_wallet_update(self, client, clubs):
+		places = 3
+		points = next((club['points'] for club in clubs if club['name'] == "Simply Lift"), None)
+		response = client.post('/purchasePlaces', data={'competition': 'Winter Showdown', 'club': 'Simply Lift', 'places': f'{places}'})
+		assert response.status_code == 200
+		assert next((club['points'] for club in clubs if club['name'] == "Simply Lift"), None) == str(int(points) - places)
+		
+
