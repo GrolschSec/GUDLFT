b53d3eb6fdbe5797a18b26698a7038cd1c8fa526
rlouvrie <rlouvrie@student.42.fr>
2024-02-13 11:57:30 +0100

    Correcting bug where a user could buy a place for an event that is past, see OpenClassrooms-Student-Center/Python_Testing#5

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import json
 from flask import Flask, render_template, request, redirect, flash, url_for
+from datetime import datetime
 
 
 def loadClubs():
@@ -12,6 +13,9 @@ def loadCompetitions():
     with open("competitions.json") as comps:
         listOfCompetitions = json.load(comps)["competitions"]
         return listOfCompetitions
+
+def is_past_competition(competition):
+    return datetime.strptime(competition["date"], "%Y-%m-%d %H:%M:%S") < datetime.now()
 
 
 app = Flask(__name__)
@@ -29,6 +33,8 @@ def index():
 @app.route("/showSummary", methods=["POST"])
 def showSummary():
     club = [club for club in clubs if club["email"] == request.form["email"]][0]
+    for competition in competitions:
+        competition.setdefault("is_past", is_past_competition(competition))
     return render_template("welcome.html", club=club, competitions=competitions)
 
 
@@ -51,9 +57,12 @@ def purchasePlaces():
         0
     ]
     club = [c for c in clubs if c["name"] == request.form["club"]][0]
-    placesRequired = int(request.form["places"])
-    competition["numberOfPlaces"] = int(competition["numberOfPlaces"]) - placesRequired
-    flash("Great-booking complete!")
+    if is_past_competition(competition):
+        flash("You can't book places for past competitions!")
+    else:
+        placesRequired = int(request.form["places"])
+        competition["numberOfPlaces"] = int(competition["numberOfPlaces"]) - placesRequired
+        flash("Great-booking complete!")
     return render_template("welcome.html", club=club, competitions=competitions)
 
 
