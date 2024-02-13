832e8c65b3448088b060dc4722fb6023219cccf6
rlouvrie <rlouvrie@student.42.fr>
2024-02-13 17:38:13 +0100

    adding a simple test for my new feature

--- tests/unit/test_server.py
+++ tests/unit/test_server.py
@@ -1,2 +1,9 @@
 import pytest
 from server import app
+
+class TestClubsPointsDisplay:
+	def test_clubs_points_display_all_clubs(self, client, clubs):
+		response = client.get('/clubs')
+		assert response.status_code == 200
+		for club in clubs:
+			assert club['name'] in response.get_data(as_text=True)
