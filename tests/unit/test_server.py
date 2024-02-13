28cb6a5a412bdc779b212c389ed7b9acd1f6a732
rlouvrie <rlouvrie@student.42.fr>
2024-02-13 10:40:17 +0100

    adding test names

--- tests/unit/test_server.py
+++ tests/unit/test_server.py
@@ -1,2 +1,9 @@
 import pytest
-from server import app
+
+class TestShowSummary:
+	def test_show_summary_past_event_not_active(self, client):
+		pass
+
+class TestPurchasePlaces:
+	def test_purchase_places_past_competition(self, client):
+		pass
