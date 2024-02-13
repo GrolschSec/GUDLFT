a486f024627b425b0265aa2b0d1a2382af7bb155
rlouvrie <rlouvrie@student.42.fr>
2024-02-13 17:45:37 +0100

    updating the test

--- tests/unit/test_server.py
+++ tests/unit/test_server.py
@@ -3,7 +3,7 @@ from server import app
 
 class TestClubsPointsDisplay:
 	def test_clubs_points_display_all_clubs(self, client, clubs):
-		response = client.get('/clubs')
+		response = client.get('/clubs/points')
 		assert response.status_code == 200
 		for club in clubs:
 			assert club['name'] in response.get_data(as_text=True)
