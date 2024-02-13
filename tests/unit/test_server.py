05755c112cdea31d161e69c49c227b0cac06787d
rlouvrie <rlouvrie@student.42.fr>
2024-02-13 10:49:48 +0100

    beginning test

--- tests/unit/test_server.py
+++ tests/unit/test_server.py
@@ -1,8 +1,11 @@
 import pytest
+from datetime import datetime
 
 class TestShowSummary:
-	def test_show_summary_past_event_not_active(self, client):
-		pass
+    def test_show_summary_past_event_not_active(self, client):
+        response = client.post("/showSummary", data={"email": "john@simplylift.co"})
+        html = response.get_data(as_text=True)
+        
 
 class TestPurchasePlaces:
 	def test_purchase_places_past_competition(self, client):
